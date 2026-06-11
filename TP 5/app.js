78ac327fe82f94cf58d5f56fe1e0b79a6b274ea8
bernii-fernandez <51085883@ivt.edu.ar>
2026-06-11 09:44:08 -0300

    tp5

---
+++ TP 5/app.js
@@ -0,0 +1,42 @@
+let numero1 = 30
+let numero2 = 20
+let suma = numero1 + numero2
+confirm("El resultado de la suma es: " + suma)
+let resta = numero1 - numero2
+confirm("El resultado de la resta es: " + resta)
+let multiplicacion = numero1 * numero2
+console.log("El resultado de la multiplicacion es: " + multiplicacion)
+let division = numero1 / numero2
+console.log("El resultado de la division es: " + division)
+
+
+
+let nombreAlumno = "Benja"
+let apellidoAlumno = "fernandez"
+let edadAlumno = 15
+console.log("El alumno : " + nombreAlumno + " " + apellidoAlumno + " tiene " + edadAlumno + " años")
+
+
+
+
+let precio1 = 1100
+let precio2 = 1500
+let precio3 = 2500
+let total = precio1 + precio2 + precio3
+console.log("El total a pagar es : " + total)
+
+
+
+
+let comidaFavorita = "Cappeletinis"
+let colorFavorito = "Negro"
+let numeroFavorito = 10
+console.log("Mi comida favorita son " + comidaFavorita + ", me gusta el color " + colorFavorito + " y mi número favorito es " + numeroFavorito)
+
+
+
+
+let pais = "argentina"
+console.log(pais)
+pais = "brasil"
+console.log(pais)
