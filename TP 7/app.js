46c3501cb4307ef1b9c79e4f92438441fb419ea8
alumno <alumno@Instituto.local>
2026-08-13 10:44:04 -0300

    234
    sdf

---
+++ TP 7/app.js
@@ -0,0 +1,9 @@
+let edad = 18
+let p_resultado = document.querySelector ("#p-resultado")
+let boton1 = document.querySelector ("#boton1")
+if (edad <= 18)
+{  
+    alert ("Eres menor de edad")
+}else{ 
+    alert ("Eres mayor de edad")
+}
